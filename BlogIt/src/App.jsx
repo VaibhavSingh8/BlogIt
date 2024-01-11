3ce1390e7642beb79d757f849fef9051eef3e5bd
Vaibhav Singh <vaibhavsinghr.08@gmail.com>
2024-01-11 20:46:13 +0000

    Project setup: initial commit

---
+++ BlogIt/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+const App=() => {
+
+  
+
+  return (
+    <>
+     <div>Hi, This is my BlogIt project!! </div>
+     <div>Jay Shree Raam !!</div>
+    </>
+  );
+}
+
+
+
+export default App
